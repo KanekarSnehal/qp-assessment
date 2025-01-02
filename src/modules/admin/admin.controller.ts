6e1ee7b3e04adb4c522da70bd59000aa2a8a1004
KanekarSnehal <snehalkanekar43@gmail.com>
2025-01-02 16:51:05 +0530

    chore: add doc type

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Patch } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { Grocery } from '../../models/grocery.model';
 
@@ -6,21 +6,37 @@ import { Grocery } from '../../models/grocery.model';
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
+  /**
+   * Add new grocery item
+   * @param grocery 
+   */
   @Post('groceries')
   async addGrocery(@Body() grocery: Partial<Grocery>) {
     return this.adminService.addGrocery(grocery);
   }
 
+  /**
+   * View all groceries
+   */
   @Get('groceries')
   async getGroceries() {
     return this.adminService.getGroceries();
   }
 
-  @Put('groceries/:id')
+  /**
+   * Update grocery item details
+   * @param id 
+   * @param updates 
+   */
+  @Patch('groceries/:id')
   async updateGrocery(@Param('id') id: number, @Body() updates: Partial<Grocery>) {
     return this.adminService.updateGrocery(id, updates);
   }
 
+  /**
+   * Remove grocery item
+   * @param id 
+   */
   @Delete('groceries/:id')
   async deleteGrocery(@Param('id') id: number) {
     return this.adminService.deleteGrocery(id);
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -6,13 +6,18 @@ import { CreateOrderDto } from './dto/create-order.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // Get a list of available grocery items
+  /**
+   * Get a list of available grocery items
+   */
   @Get('groceries')
   async getAvailableGroceries() {
     return this.userService.getAvailableGroceries();
   }
 
-  // Book multiple grocery items in a single order
+  /**
+   * Book multiple grocery items in a single order
+   * @param createOrderDto 
+   */
   @Post('orders')
   async createOrder(@Body() createOrderDto: CreateOrderDto) {
     return this.userService.createOrder(createOrderDto);
